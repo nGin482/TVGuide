81e33cb1db68dec679256c1f561acb3e0656b7d8
Nicholas Giumelli <nicholasgiumelli@gmail.com>
2020-12-22 17:09:46 +1100

    New internal logging for emails

--- log.py
+++ log.py
@@ -1,34 +1,23 @@
-from datetime import datetime
-import os
-
-
-def get_log_file():
-    log_file = os.path.join('C:\\Users', 'Nicholas', 'PycharmProjects', 'emails.txt')
-
-    return log_file
+from datetime import datetime as dt
 
 
 def get_date_from_latest_email():
 
-    file = get_log_file()
-
     arr = []
-    with open(file) as f:
+    
+    with open('log/emails.txt') as f:
         for line in f:
-            if 'TVGuide' in line:
-                if '\n' in line:
-                    arr.append(line[:-1])
-                else:
-                    arr.append(line)
+            if '\n' in line:
+                arr.append(line[:-1])
+            else:
+                arr.append(line)
 
     latest_email = arr[-1]
-    print(latest_email)
-
     idx_front = latest_email.find('on ')
     idx_back = latest_email.find(' at ')
-    date = latest_email[idx_front + 3:idx_back]
-    time = latest_email[idx_back + 4:].split(':')
-    date_parsed = datetime.strptime(date, '%d-%m-%y')
+    date = latest_email[idx_front+3:idx_back]
+    time = latest_email[idx_back+4:].split(':')
+    date_parsed = dt.strptime(date, '%d-%m-%y')
     new_date_parsed = date_parsed.replace(hour=int(time[0]), minute=int(time[1]))
 
     print(new_date_parsed)
@@ -38,39 +27,47 @@ def get_date_from_latest_email():
 def compare_dates():
 
     date = get_date_from_latest_email()
-    if date.day != datetime.today().day:
+    if date.day != dt.today().day:
         return True
     else:
-        return False
+        if date.hour <= 6:
+            return True
+        else:
+            return False
 
 
-def read_log_file():
+def read_file():
 
-    file = get_log_file()
+    with open('log/emails.txt') as fd:
+        data = fd.read()
 
-    with open(file) as f:
-        content = f.read()
+    return data
 
-    return content
+
+def write_to_backup():
+    contents = read_file()
+
+    with open('log/backup_log.txt', 'w') as fd:
+        fd.write(contents)
 
 
 def write_to_log_file():
 
-    file = get_log_file()
-    message = read_log_file() + 'TVGuide was sent on ' + datetime.strftime(datetime.today(), '%d-%m-%y') + ' at ' + \
-        datetime.strftime(datetime.today(), '%H:%M')
-    with open(file, 'w') as fd:
-        fd.write(message + '\n')
+    write_to_backup()
+    
+    contents = read_file().splitlines(True)
+    if len(contents) > 1:
+        new_log = [contents[1]]
+    new_log.append('\nTVGuide was sent on ' + dt.strftime(dt.today(), '%d-%m-%y') + ' at ' + dt.strftime(dt.today(), '%H:%M'))
+    
+    with open('log/emails.txt', 'w') as fd:
+        for line in new_log:
+            fd.write(line)
 
 
 def delete_latest_entry():
+    with open('log/backup_log.txt') as fd:
+        backup = fd.read()
 
-    contents = read_log_file()
-    lines = contents.splitlines(True)
-    lines.pop(-1)
-    log_string = ''
-    for line in lines:
-        log_string = log_string + line
-
-    with open(get_log_file(), 'w') as fd:
-        fd.write(log_string)
+    with open('log/emails.txt', 'w') as fd:
+        fd.write(backup)
