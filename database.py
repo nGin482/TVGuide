8b380f2349e634ee74ee483d01f390ade819191b
nGin482 <ngin2547@gmail.com>
2021-07-13 17:03:18 +1000

    Added DB handlers to: - get all shows - get one show - remove show from list
    Error handing when inserting and deleting from DB

--- database.py
+++ database.py
@@ -13,11 +13,35 @@ def showlist_collection():
     showlist = database().ShowList
     return showlist
 
+def get_showlist():
+    list_of_shows = []
+    for show in showlist_collection().find():
+        list_of_shows.append(show['show'])
+    return list_of_shows
+
+def find_show(show_to_find):
+    list_of_shows = get_showlist()
+    for show in list_of_shows:
+        if show_to_find in show:
+            return {'status': True, 'show': show}
+    return {'status': False, 'message': 'The show given could not be found in the database.'}
+
 def insert_into_showlist_collection(show):
-    show_document = {
-        "show": show
-    }
-    showlist_collection().insert_one(show_document)
+    try:
+        show_document = {
+            "show": show
+        }
+        showlist_collection().insert_one(show_document)
+        return {'status': True, 'message': show + ' has been added to the show list.'}
+    except Exception:
+        return {'status': False, 'message': ' There was a problem adding ' + show + ' to the show list.'}
+
+def remove_show_from_list(show_to_remove):
+    try:
+        showlist_collection().delete_one({'show': show_to_remove})
+        return {'status': True, 'message': show_to_remove + ' has been removed from the show list.'}
+    except Exception:
+        return {'status': False, 'message': ' There was a problem removing ' + show_to_remove + ' from the show list.'}
 
 def recorded_shows_collection():
     recorded_shows = database().RecordedShows
