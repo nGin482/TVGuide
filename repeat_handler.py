6519feb010b8de11082fc973ee09f4fbcea63258
nGin482 <ngin2547@gmail.com>
2021-07-27 13:03:00 +1000

    Remove previous process of setting repeats and adding shows/seasons/episodes/channels

--- repeat_handler.py
+++ repeat_handler.py
@@ -52,126 +52,7 @@ def convert_to_objects(superlist):
     return json_list
 
 
-def special_cases(show):
-    if 'Maigret' in show['title']:
-        return 'shows/Maigret.json'
-    if 'Revenge Of The Fallen' in show['title'] or 'Dark Of The Moon' in show['title'] \
-            or 'Age of Extinction' in show['title'] or 'The Last Knight' in show['title']:
-        return 'shows/Transformers.json'
-    elif show['title'] == 'Transformers':
-        return 'shows/Transformers.json'
-    else:
-        title = show['title']
-        if ':' in title:
-            idx = title.rfind(':')
-            title = title[:idx] + title[idx+1:]
-        return 'shows/' + title + '.json'
-
-
-def read_file(show):
-
-    title = show['title']
-    if 'Transformers' in title or 'Maigret' in title:
-        file_name = special_cases(show)
-    else:
-        if ':' in title:
-            idx = title.rfind(':')
-            title = title[:idx] + title[idx+1:]
-        file_name = 'shows/' + title + '.json'
-
-    try:
-        with open(file_name, 'r', encoding='utf-8') as fd:
-            file_string = fd.read()
-            data = json.loads(file_string)
-    except FileNotFoundError:
-        with open(file_name, 'w+', encoding='utf-8') as fd:
-            data = [{
-                'season num': 'Unknown',
-                'episodes': [{
-                    'episode num': '',
-                    'episode title': '',
-                    'channels': [show['channel']],
-                    'first air date': date.today().strftime('%d-%m-%Y'),
-                    'repeat': False
-                }]
-            }]
-            if 'series_num' in show.keys():
-                data[0]['season num'] = show['series_num']
-            if 'episode_num' in show.keys():
-                data[0]['episodes'][0]['episode num'] = show['episode_num']
-            if 'episode_title' in show.keys():
-                data[0]['episodes'][0]['episode title'] = show['episode_title']
-            json.dump(data, fd, ensure_ascii=False, indent=4)
-            fd.close()
-
-    return data
-
-
 def flag_repeats(show):
-
-    file = read_file(show)
-    if 'series_num' in show.keys():
-        season_found = -1
-        for season in file:
-            if season['season num'] == show['series_num']:
-                season_found = 1
-                episodes = season['episodes']
-                episode_found = -1
-                for episode in episodes:
-                    if episode['episode num'] == show['episode_num']:
-                        episode_found = 1
-                        if episode['first air date'] != date.today().strftime('%d-%m-%y'):
-                            if show['channel'] in episode['channels']:
-                                if 'ABC1' in show['channel']:
-                                    episode['channels'].append('ABCHD')
-                                if '10' in show['channel']:
-                                    episode['channels'].append('TENHD')
-                                episode['repeat'] = True
-                            else:
-                                if 'ABC1' in show['channel']:
-                                    episode['channels'].append('ABCHD')
-                                if '10' in show['channel']:
-                                    episode['channels'].append('TENHD')
-                                episode['channels'].append(show['channel'])
-                                episode['repeat'] = True
-                if episode_found == -1:
-                    obj = {'episode num': show['episode_num'], 'episode title': '', 'channels': [show['channel']],
-                           'first air date': date.strftime(date.today(), '%d-%m-%Y'), 'repeat': False}
-                    if 'episode_title' in show.keys():
-                        obj['episode title'] = show['episode_title']
-                    season['episodes'].append(obj)
-        if season_found == -1:
-            obj = {'season num': show['series_num'], 'episodes': [
-                {'episode num': show['episode_num'], 'episode title': '', 'channels': [show['channel']],
-                 'first air date': date.strftime(date.today(), '%d-%m-%Y'), 'repeat': False}]}
-            if 'episode_title' in show.keys():
-                obj['episodes'][0]['episode title'] = show['episode_title']
-            if 'ABC1' in show['channel']:
-                obj['episodes'][0]['channels'].append('ABCHD')
-            if '10' in show['channel']:
-                obj['episodes'][0]['channels'].append('TENHD')
-            file.append(obj)
-
-    else:
-        for season in file:
-            episodes = season['episodes']
-            found = -1
-            for episode in episodes:
-                if episode['episode title'] == show['episode_title']:
-                    found = 1
-                    if episode['first air date'] != date.today().strftime('%d-%m-%y'):
-                        if show['channel'] in episode['channels']:
-                            episode['repeat'] = True
-                        else:
-                            episode['channels'].append(show['channel'])
-                            episode['repeat'] = True
-            if found == -1:
-                season['episodes'].append({'episode num': '', 'episode title': show['episode_title'],
-                                           'channels': [show['channel']],
-                                           'first air date': date.strftime(date.today(), '%d-%m-%Y'),
-                                           'repeat': False})
-
-    write_back_to_file({'title': show['title'], 'seasons': file})
 
     show['title'] = check_show_titles(show)
     check_episode = find_recorded_episode(show)
@@ -191,16 +72,6 @@ def flag_repeats(show):
             return insert_show
         else:
             return {'status': False, 'message': 'Unable to process this episode.'}
-
-def write_back_to_file(data):
-
-    title = data['title']
-    if ':' in title:
-        idx = title.rfind(':')
-        title = title[:idx] + title[idx+1:]
-    with open('shows/' + title + '.json', 'w', encoding='utf-8') as fd:
-        json.dump(data['seasons'], fd, ensure_ascii=False, indent=4)
-
 
 def find_recorded_episode(show):
     check_show = get_one_recorded_show(show['title'])
