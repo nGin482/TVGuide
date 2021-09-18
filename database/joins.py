5d660451c7056ca7685b37d0bd3891c9075e2145
nGin482 <ngin2547@gmail.com>
2021-09-18 13:38:04 +1000

    Update to getting a User's reminders following change to retriving Reminders by ID
    Remove a Reminder completely from the database

--- database/joins.py
+++ database/joins.py
@@ -1,5 +1,5 @@
-from database.users_collection import get_user
-from database.reminder_collection import get_reminder_by_id
+from database.users_collection import get_user, remove_reminder_for_user
+from database.reminder_collection import reminders_collection, get_reminder_by_id, remove_reminder_by_ID
 
 def get_reminders_for_user(user: str):
     """Retrieve all Reminder documents allocated to a given user, using the reminderIDs from the User document
@@ -15,7 +15,22 @@ def get_reminders_for_user(user: str):
     user = get_user(user)
     if user['status']:
         reminderIDs: list = user['user']['reminders']
-        user_reminders = [get_reminder_by_id(reminderID) for reminderID in reminderIDs]
+        user_reminders = []
+        
+        for reminderID in reminderIDs:
+            reminder_status = get_reminder_by_id(reminderID)
+            if reminder_status['status']:
+                user_reminders.append(reminder_status['reminder'])
+        
         return user_reminders
     else:
         return []
+
+def remove_reminder_from_database(user: str, reminderID: str) -> dict:
+    """
+    Remove a `Reminder` from the database by:\n
+    - Removing it from the `User`'s document\n
+    - Removing it from the `Reminders` collection
+    """
+    remove_reminder_for_user(user, reminderID)
+    remove_reminder_by_ID(reminderID)
