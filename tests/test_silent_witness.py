8c8cae454846faa0fe14b4c6dca8755f1e6d2196
nGin482 <ngin2547@gmail.com>
2022-08-19 23:13:29 +1000

    Tests for Silent Witness episodes

--- tests/test_silent_witness.py
+++ tests/test_silent_witness.py
@@ -3,21 +3,10 @@ from database.models.GuideShow import SilentWitness
 
 class TestSilentWitness(unittest.TestCase):
 
-    def setUp(self) -> None:
-        self.example_shows = ["Brother's Keeper - Part 1", "Bad Love (Part 2)", "Maters of Life and Death Part 1", "Redemption Part 1"]
-        self.silent_witness_shows = [SilentWitness.handle('', 0, episode_title) for episode_title in self.example_shows]
-    
-    
-    
     def test_silent_witness(self):
-        example_shows = ["Brother's Keeper - Part 1", "Bad Love (Part 2)", "Maters of Life and Death Part 1", "Redemption Part 1"]
-        self.assertEqual(sum([1, 2, 3]), 6)
-        silent_witness_shows = [SilentWitness.handle('', 0, episode_title) for episode_title in example_shows]
-        
-        first_show = silent_witness_shows[0]
-        second_show = silent_witness_shows[1]
-        third_show = silent_witness_shows[2]
-        fourth_show = silent_witness_shows[3]
+        first_show = SilentWitness.handle('', 0, "Brother's Keeper - Part 1")
+        second_show = SilentWitness.handle('', 0, "Bad Love (Part 2)")
+        fourth_show = SilentWitness.handle('', 0, "Redemption Part 1")
          
         self.assertIsInstance(first_show, tuple)
         self.assertEqual(5, len(first_show))
@@ -31,6 +20,12 @@ class TestSilentWitness(unittest.TestCase):
         self.assertEqual(4, second_show[3])
         self.assertEqual("Bad Love (Part 2)", second_show[4])
 
-        with self.assertRaises(ValueError):
-            SilentWitness.handle('', 0, "Maters of Life and Death Part 1")
+        self.assertRaises(ValueError, SilentWitness.handle, '', 0, "Maters of Life and Death Part 1")
+
+        self.assertIsInstance(fourth_show, tuple)
+        self.assertEqual(5, len(fourth_show))
+        self.assertEqual('24', fourth_show[2])
+        self.assertEqual(1, fourth_show[3])
+        self.assertEqual("Redemption Part 1", fourth_show[4])
+            
         # self.assertEqual('Episode not in Season 24', str(ctx.exception))
