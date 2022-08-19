57a9544704ef7eb58f7a1d0f32b322b1953ff8bb
nGin482 <ngin2547@gmail.com>
2022-08-19 21:59:37 +1000

    Fixing issue with logging

--- log.py
+++ log.py
@@ -187,5 +187,5 @@ def log_silent_witness_episode(silent_witness: dict):
     silent_witness['time'] = datetime.strptime(silent_witness['time'], '%H:%M')
 
 def logging_app(log_info: str, level = logging.DEBUG):
-    logging.basicConfig(filename='tvguide.log', filemode='w', level=level, format='%(asctime)s - %(levelname)s - %(msg)')
+    logging.basicConfig(filename='tvguide.log', filemode='w', level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
     logging.info(log_info)
