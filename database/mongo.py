89ece3ba6f68aa5975c6343972a76fc9c998c08b
nGin482 <ngin2547@gmail.com>
2022-09-15 17:01:58 +1000

    Collections now available in mongo module Method to delete a reminder

--- database/mongo.py
+++ database/mongo.py
@@ -16,3 +16,27 @@ def database():
         return mongo_client.get_database('tvguide')
     else:
         return None
+
+def showlist_collection():
+    db = database()
+    if db is not None:
+        showlist = db.get_collection('ShowList')
+        return showlist
+    else:
+        return []
+
+def recorded_shows_collection():
+    db = database()
+    if db is not None:
+        recorded_shows = db.get_collection('RecordedShows')
+        return recorded_shows
+    else:
+        return []
+
+def reminders_collection():
+    db = database()
+    if db is not None:
+        reminders = db.get_collection('Reminders')
+        return reminders
+    else:
+        return []
