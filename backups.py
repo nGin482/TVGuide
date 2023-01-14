34b9b96cd082741c580793dacb2e8dc44e4ec5e4
nGin482 <ngin2547@gmail.com>
2023-01-14 12:08:07 +1100

    Reworking BackUps logic to use f-strings

--- backups.py
+++ backups.py
@@ -4,16 +4,6 @@ import json
 import os
 
 
-def get_data(obj_list):
-    json_list = []
-
-    for obj in obj_list:
-        obj_dict = obj.to_dict()
-        json_list.append(obj_dict)
-
-    return json_list
-
-
 def create_monthly_folder():
     today = date.today()
     if today.day == 1:
@@ -33,45 +23,37 @@ def create_monthly_folder():
 def move_month_files():
     if date.today().day == 1:
         create_monthly_folder()
-        i = 16
         prev_month = date.today() - timedelta(days=1)
-        previous_month_folder = 'BackUps/' + prev_month.strftime('%B-%Y')
-        day_range = calendar.monthrange(prev_month.year, prev_month.month)[1]
-        while i <= day_range:
+        previous_month_folder = f'BackUps/{prev_month.strftime("%B-%Y")}'
+        for i in range(16, calendar.monthrange(prev_month.year, prev_month.month)[1]):
             day = str(i)
-            file = day + '-' + date.strftime(prev_month, '%m-%Y') + '.json'
+            file = f'{day}-{date.strftime(prev_month, "%m-%Y")}.json'
             original_path = os.path.join('BackUps', file)
             if os.path.exists(original_path) and os.path.exists(previous_month_folder):
                 os.rename(original_path, os.path.join(previous_month_folder, file))
-            i += 1
 
     if date.today().day == 16:
-        i = 1
-        while i < 16:
-            day = str(i)
-            if len(day) == 1:
-                day = '0' + day
-            file = day + '-' + date.strftime(date.today(), '%m-%Y') + '.json'
+        for i in range(1, 16):
+            day = str(i) if i >= 10 else f'0{str(i)}'
+            file = f'{day}-{date.strftime(date.today(), "%m-%Y")}.json'
             original_path = os.path.join('BackUps', file)
-            new_path = 'BackUps/' + date.strftime(date.today(), '%B-%Y')
+            new_path = f'BackUps/{date.strftime(date.today(), "%B-%Y")}'
             if os.path.exists(original_path) and os.path.exists(new_path):
                 os.rename(original_path, os.path.join(new_path, file))
-            i += 1
 
     if date.today().day == 1 and date.today().month == 1:
         create_monthly_folder()
-        prev_year = date((date.today() - timedelta(days=1)).year, 1, 1)
-        for month in calendar.month_name:
-            if month != '':  # first element is empty string
-                original_directory = 'BackUps/' + month + '-' + str(prev_year.year)
-                new_directory = 'BackUps/' + prev_year.strftime('%Y') + '/' + month + '-' + str(prev_year.year)
-                os.rename(original_directory, new_directory)
+        prev_year = str(date.today().year - 1)
+        for directory in os.listdir('BackUps'):
+            if prev_year in directory:
+                new_directory = f'BackUps/{prev_year}/{directory}'
+                os.rename(f'BackUps/{directory}', new_directory)
     print(calendar.month_name[0:])
 
 
 def write_to_backup_file(backup_guide):
 
     move_month_files()
-    filename = 'BackUps/' + date.strftime(date.today(), '%d-%m-%Y') + '.json'
+    filename = f'BackUps/{date.strftime(date.today(), "%d-%m-%Y")}.json'
     with open(filename, 'w', encoding='utf-8') as fd:
         json.dump(backup_guide, fd, ensure_ascii=False, indent=4)
