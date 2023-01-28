95cb1af98820011faaa40c0ca5d46d0aab2fc18c
nGin482 <ngin2547@gmail.com>
2023-01-29 10:49:05 +1100

    Changes needed to accommodate models feature

--- main.py
+++ main.py
@@ -1,24 +1,24 @@
-
-from aux_methods.helper_methods import format_time, show_list_for_message, remove_doubles, check_show_titles, show_string
-from aux_methods.episode_info import search_episode_information
-from data_validation.validation import Validation
-from database.models.GuideShow import GuideShow
-from database.show_list_collection import search_list, insert_into_showlist_collection, remove_show_from_list
-from database.recorded_shows_collection import backup_recorded_shows
-from exceptions.BBCNotCollectedException import BBCNotCollectedException
-from repeat_handler import get_today_shows_data
-from log import clear_imdb_api_results, log_discord_message_too_long, log_message_sent, compare_dates, log_guide, revert_tvguide
-from datetime import datetime
-from dotenv import load_dotenv
 from bs4 import BeautifulSoup
+from datetime import datetime
+from discord import Client, Message, TextChannel
+from dotenv import load_dotenv
 from requests import get
-from reminders import *
-import discord
 import os
 import re
 
+from aux_methods.helper_methods import format_time, show_list_for_message, check_show_titles
+from aux_methods.episode_info import search_episode_information
+from data_validation.validation import Validation
+from database.DatabaseService import DatabaseService
+from database.mongo import mongo_client
+from exceptions.BBCNotCollectedException import BBCNotCollectedException
+from exceptions.DatabaseError import DatabaseError, SearchItemAlreadyExistsError, SearchItemNotFoundError
+from guide import run_guide, revert_tvguide
+from log import log_discord_message_too_long, log_message_sent
+from repeat_handler import get_today_shows_data
+
 load_dotenv('.env')
-client = discord.Client()
+discord_client = Client()
 
 # https://epg.abctv.net.au/processed/events_Sydney_vera.json
 # https://www.abc.net.au/tv/programs/vera/series-episode-index.json?_=1555488755177
@@ -112,82 +112,6 @@ def search_vera_series():
     return series_num
 
 
-def search_free_to_air():
-    """
-
-    :return:
-    """
-
-    current_date = datetime.today().date()
-    new_url = 'https://epg.abctv.net.au/processed/Sydney_' + str(current_date) + ".json"
-    shows_on: list[GuideShow] = []
-    shows_data: list[dict] = []
-
-    data = find_json(new_url)['schedule']
-
-    for item in data:
-        listing = item['listing']
-
-        # print("Listing: " + str(listing))
-        for guide_show in listing:
-            title = guide_show['title']
-            for show in show_list:
-                if show in title:
-                    show_date = guide_show['start_time'][:-9]
-                    if int(show_date[-2:]) == int(datetime.today().day):
-                        episode_info = False
-                        season_number = ''
-                        episode_number = 0
-                        episode_title = ''
-                        if 'series_num' in guide_show.keys() and 'episode_num' in guide_show.keys():
-                            episode_info = True
-                            season_number = str(guide_show['series_num'])
-                            episode_number = int(guide_show['episode_num'])
-                            if 'episode_title' in guide_show.keys():
-                                episode_title = guide_show['episode_title']
-                        if 'episode_title' in guide_show.keys():
-                            episode_info = True
-                            episode_title = guide_show['episode_title']
-                        shows_data.append({
-                            'title': guide_show['title'],
-                            'channel': item['channel'],
-                            'time': datetime.strptime(guide_show['start_time'][-8:-3], '%H:%M'),
-                            'episode_info': episode_info,
-                            'season_number': season_number,
-                            'episode_number': episode_number,
-                            'episode_title': episode_title
-                        })
-
-    shows_data = Validation.remove_unwanted_shows(shows_data)
-    show_titles = [show['title'] for show in shows_data]
-    get_today_shows_data(show_titles)
-    
-    shows_on = [
-        GuideShow(
-            data['title'],
-            data['channel'],
-            data['time'],
-            data['episode_info'],
-            data['season_number'],
-            data['episode_number'],
-            data['episode_title']
-        ) for data in shows_data
-    ]
-    shows_on = [show.search_imdb_information() for show in shows_on]
-    shows_on.sort(key=lambda show_obj: show_obj.time)
-    # check = check_time_sort(shows_on)
-    # while check[0] != -1 and check[1] != -1:
-    #     sort_shows_by_time(shows_on, check[0], check[1])
-    #     check = check_time_sort(shows_on)
-    remove_doubles(shows_on)
-    # # if imdb_api_status == 200:
-    # #     shows_on = [search_episode_information(show) for show in shows_on]
-    # else:
-    #     print('IMDB API is not available')
-
-    return shows_on
-
-
 def search_bbc_channels():
     """
 
@@ -272,154 +196,107 @@ def search_bbc_channels():
     return shows_on
 
 
-def check_site():
-    """
-
-    :return: a dictionary of shows on, where key is based on url and value is the list of scheduled shows
-    """
-
-    shows_on = {'BBC': search_bbc_channels(),
-                'FTA': search_free_to_air(),
-                'Latest Vera Series': search_vera_series()
-                }
-
-    return shows_on
-
-
-def compose_message():
-    """
-    toString function that writes the shows, times, channels and episode information (if available) via natural language
-    :return: the to-string message
-    """
-    weekdays = [
-        'Monday',
-        'Tuesday',
-        'Wednesday',
-        'Thursday',
-        'Friday',
-        'Saturday',
-        'Sunday',
-    ]
-
-    message_date = datetime.today().date()
-    message = weekdays[message_date.weekday()] + " " + str(message_date.strftime('%d-%m-%Y')) + " TVGuide\n"
-
-    # Free to Air
-    message = message + "\nFree to Air:\n"
-    if len(fta_shows) == 0:
-        message = message + "Nothing on Free to Air today\n"
-    else:
-        for show in fta_shows:
-            message += show.message_string()
-
-    # BBC
-    message = message + "\nBBC:\n"
-    if len(bbc_shows) == 0 or bbc_shows[0] is []:
-        message = message + "Nothing on BBC today\n"
-    else:
-        for show in bbc_shows:
-            show['time'] = show['time'].strftime('%H:%M')
-            message = message + show_string(show)
-
-    return message
-
-
 async def send_message():
     """
 
     :param send_status:
     :return: n/a
     """
-    message = compose_message()
-    print(message)
+    message = run_guide(database_service)
     
-    if status:
-        await client.wait_until_ready()
-        tvguide_channel = client.get_channel(int(os.getenv('TVGUIDE_CHANNEL')))
-        try:
-            if len(message) > 2000:
-                bbc_idx = message.find("BBC: ")
+    await discord_client.wait_until_ready()
+    tvguide_channel: TextChannel = discord_client.get_channel(int(os.getenv('TVGUIDE_CHANNEL')))
+    try:
+        if len(message) > 2000:
+            bbc_idx = message.find("BBC: ")
 
-                fta_message = message[:bbc_idx]
-                bbc_message = message[bbc_idx:]
+            fta_message = message[:bbc_idx]
+            bbc_message = message[bbc_idx:]
 
-                if len(fta_message) > 2000:
-                    print(len(fta_message))
-                    
-                    fta_pm_idx = re.search('[1][2]:[0-5][0-9]:', fta_message).start()
-                    
-                    fta_am_message = fta_message[:fta_pm_idx] +  '\n\n'
-                    fta_pm_message = fta_message[fta_pm_idx:]
-
-                    await tvguide_channel.send(fta_am_message)
-                    await tvguide_channel.send(fta_pm_message)
-                    # await tvguide_channel.send(bbc_message)
-
-                    log_discord_message_too_long(len(message), len(fta_message))
-                else:
-                    await tvguide_channel.send(fta_message)
-                    await tvguide_channel.send(bbc_message)
-
-
+            if len(fta_message) > 2000:
+                print(len(fta_message))
                 
-                # still too big on Thursday 18/11/21
+                fta_pm_idx = re.search('12:[0-5][0-9]:', fta_message).start()
+                
+                fta_am_message = fta_message[:fta_pm_idx] + '\n\n'
+                fta_pm_message = fta_message[fta_pm_idx:]
+
+                await tvguide_channel.send(fta_am_message)
+                await tvguide_channel.send(fta_pm_message)
+                # await tvguide_channel.send(bbc_message)
+
+                log_discord_message_too_long(len(message), len(fta_message))
             else:
-                await tvguide_channel.send(message)
-        except AttributeError:
-            ngin = await client.fetch_user(int(os.getenv('NGIN')))
-            await ngin.send('The channel resolved to NoneType so the message could not be sent')
-        log_message_sent()
-        log_guide(fta_shows, bbc_shows)
+                await tvguide_channel.send(fta_message)
+                await tvguide_channel.send(bbc_message)
+
+
+            
+            # still too big on Thursday 18/11/21
+        else:
+            await tvguide_channel.send(message)
+    except AttributeError:
+        ngin = await discord_client.fetch_user(int(os.getenv('NGIN')))
+        await ngin.send('The channel resolved to NoneType so the message could not be sent')
+    log_message_sent()
+    # log_guide(fta_shows, bbc_shows)
     
-    await client.close()
+    await discord_client.close()
 
-@client.event
+@discord_client.event
 async def on_ready():
-    print('Logged in as', client.user)
+    print('Logged in as', discord_client.user)
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
+@discord_client.event
+async def on_message(message: Message):
+    if message.author == discord_client.user:
         return
     if 'tv-guide' in str(message.channel):
         if '$show-list' in message.content:
-            await message.channel.send(show_list_for_message(show_list))
+            await message.channel.send(show_list_for_message(database_service.get_search_list()))
         if '$add-show' in message.content:
             new_show = message.content.split(' ')[1]
-            insert_into_showlist_collection(new_show)
-            new_message = new_show + ' has been added to the list. The list now includes:\n' + show_list_for_message(show_list)
-            await message.channel.send(new_message)
+            try:
+                database_service.insert_into_showlist_collection(new_show)
+                reply = f'{new_show} has been added to the list. The list now includes:\n{show_list_for_message(database_service.get_search_list())}'
+            except SearchItemAlreadyExistsError | DatabaseError as err:
+                reply = f'Error: {str(err)}. The List has not been modified.'
+            await message.channel.send(reply)
         if '$remove-show' in message.content:
             show_to_remove = message.content[message.content.index('-show')+6:]
-            remove_show = remove_show_from_list(show_to_remove)
-            if remove_show['status']:
-                reply = remove_show['message'] + ' The list now includes:\n' + show_list_for_message(show_list)
-            else:
-                reply = remove_show['message'] + ' The list remains as:\n' + show_list_for_message(show_list)
+            try:
+                database_service.remove_show_from_list(show_to_remove)
+                reply = f'{show_to_remove} has been removed from the SearchList. The list now includes:\n' + show_list_for_message(database_service.get_search_list())
+            except DatabaseError | SearchItemNotFoundError as err:
+                reply = f'{str(err)}. The list remains as:\n' + show_list_for_message(database_service.get_search_list())
+
             await message.channel.send(reply)
 
 
 if __name__ == '__main__':
-    status = compare_dates()
-    print(status)
-    show_list = search_list()
-    imdb_api_status = get('https://imdb-api.com/').status_code
-    fta_shows = search_free_to_air()
-    bbc_shows = search_bbc_channels()
-
-    backup_recorded_shows()
-    clear_imdb_api_results()
     
-    client.loop.create_task(send_message())
-    client.run(os.getenv('HERMES'))
+    database_service = DatabaseService(mongo_client().get_database('tvguide'))
+
+    # status = compare_dates()
+    # print(status)
+    # show_list = search_list()
+    imdb_api_status = get('https://imdb-api.com/').status_code
+    # clear_imdb_api_results()
+    # RecordedShow.backup_recorded_shows()
+    # fta_shows = search_free_to_air()
+    # bbc_shows = search_bbc_channels()
+
+    discord_client.loop.create_task(send_message())
+    discord_client.run(os.getenv('HERMES'))
     # print(compose_message())
 
     # print(reminders_found())
     # check_reminders_interval()
     # compare_reminder_interval()
-    calculate_reminder_time()
 
-    revert_tvguide()
+    # calculate_reminder_time(show_list)
+
+    # revert_tvguide(database_service)
 
     # add_show_to_list('Baptiste')
     # delete_latest_entry()
