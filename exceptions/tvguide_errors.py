5a82b4dd52db2f97e541ba04923c8f52356c1c76
nGin482 <ngin2547@gmail.com>
2023-02-03 18:21:26 +1100

    New exception for messages not found BBCNotCollected moved with MessageNotFound

--- exceptions/tvguide_errors.py
+++ exceptions/tvguide_errors.py
@@ -1,3 +1,9 @@
+class MessageNotFoundException(Exception):
+    """
+    Raised when a TVGuide message could not be found
+    """
+    pass
+
 class BBCNotCollectedException(Exception):
     """
     Raised when the BBC page prevents the scraper from collecting guide data
