fac65998e01b5dc0b83130386dea67f4317e95c6
nGin482 <ngin2547@gmail.com>
2023-02-19 12:09:04 +1100

    Commands to see details about a RecordedShow and Episodes

--- services/notifications.py
+++ services/notifications.py
@@ -4,7 +4,7 @@ from discord.ext.commands import Bot, Context, DefaultHelpCommand
 from dotenv import load_dotenv
 
 from aux_methods.helper_methods import show_list_message, parse_date_from_command
-from exceptions.DatabaseError import DatabaseError, SearchItemAlreadyExistsError, SearchItemNotFoundError
+from exceptions.DatabaseError import DatabaseError, SearchItemAlreadyExistsError, SearchItemNotFoundError, SeasonNotFoundError, ShowNotFoundError
 from config import database_service
 from log import get_date_from_tvguide_message
 from guide import compose_message, revert_database_tvguide
@@ -75,3 +75,20 @@ async def revert_tvguide(ctx: Context, date_to_delete: str = None):
     else:
         await ctx.send('The message to delete could not be found')
     await ctx.send('The TVGuide has been reverted.')
+
+@hermes.command()
+async def recorded_show(ctx: Context, show: str):
+    try:
+        show_record = database_service.get_one_recorded_show(show)
+        await ctx.send(show_record.message_format())
+    except ShowNotFoundError as err:
+        await ctx.send(f'Error: {str(err)}')
+
+@hermes.command()
+async def season_details(ctx: Context, show: str, season: str):
+    try:
+        show_record = database_service.get_one_recorded_show(show)
+        season_record = show_record.find_season(season)
+        await ctx.send(season_record.message_format())
+    except (ShowNotFoundError, SeasonNotFoundError) as err:
+        await ctx.send(f'Error: {str(err)}')
