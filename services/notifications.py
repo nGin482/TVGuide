53c982360d94435b7fd464d5decd7d62a5774a08
nGin482 <ngin2547@gmail.com>
2023-02-25 12:49:16 +1100

    Commands to interact with Reminders Update to season_details command

--- services/notifications.py
+++ services/notifications.py
@@ -4,10 +4,12 @@ from discord.ext.commands import Bot, Context, DefaultHelpCommand
 from dotenv import load_dotenv
 
 from aux_methods.helper_methods import show_list_message, parse_date_from_command
-from exceptions.DatabaseError import DatabaseError, SearchItemAlreadyExistsError, SearchItemNotFoundError, SeasonNotFoundError, ShowNotFoundError
+from database.models.Reminders import Reminder
+from exceptions.DatabaseError import DatabaseError, ReminderNotFoundError, SearchItemAlreadyExistsError, SearchItemNotFoundError, ShowNotFoundError
 from config import database_service
 from log import get_date_from_tvguide_message
 from guide import compose_message, revert_database_tvguide
+from data_validation.validation import Validation
 
 load_dotenv('.env')
 hermes = Bot(command_prefix='$', help_command=DefaultHelpCommand())
@@ -89,6 +91,54 @@ async def season_details(ctx: Context, show: str, season: str):
     try:
         show_record = database_service.get_one_recorded_show(show)
         season_record = show_record.find_season(season)
-        await ctx.send(season_record.message_format())
-    except (ShowNotFoundError, SeasonNotFoundError) as err:
+        if season_record is not None:
+            await ctx.send(season_record.message_format())
+        else:
+            if season == 'Unknown':
+                await ctx.send(f'{show} does not have an {season} season')
+            else:
+                await ctx.send(f'{show} does not have {season} seasons')
+    except ShowNotFoundError as err:
+        await ctx.send(f'Error: {str(err)}')
+
+@hermes.command()
+async def create_reminder(ctx: Context, show: str, reminder_alert: str = 'Before', warning_time: str = '3', occassions: str = 'All'):
+    try:
+        reminder_exists = database_service.get_one_reminder(show)
+        if reminder_exists is not None:
+            await ctx.send(f'A reminder already exists for {show}')
+    except ReminderNotFoundError:
+        reminder = Reminder.from_values(show, reminder_alert, int(warning_time), occassions)
+        database_service.insert_new_reminder(reminder)
+        await ctx.send(f'A reminder has been created for {show}')
+
+@hermes.command()
+async def view_reminder(ctx: Context, show: str):
+    try:
+        reminder = database_service.get_one_reminder(show)
+        await ctx.send(reminder.message_format())
+    except ReminderNotFoundError as err:
+        await ctx.send(f'Error: {err}')
+
+@hermes.command()
+async def update_reminder(ctx: Context, show: str, attribute: str, value: str):
+    try:
+        reminder = database_service.get_one_reminder(show)
+        if attribute in Validation.valid_reminder_fields():
+            if attribute == 'warning_time':
+                value = int(value)
+            setattr(reminder, attribute, value)
+            database_service.update_reminder(reminder)
+            await ctx.send(f"The reminder for {show} has been updated. It's details are now:\n{reminder.message_format()}")
+        else:
+            await ctx.send(f'Error: {attribute} is not a valid property for a reminder')
+    except ReminderNotFoundError as err:
+        await ctx.send(f'Error: {str(err)}')
+
+@hermes.command()
+async def delete_reminder(ctx: Context, show: str):
+    try:
+        database_service.delete_reminder(show)
+        await ctx.send(f'The Reminder for {show} has been removed')
+    except ReminderNotFoundError as err:
         await ctx.send(f'Error: {str(err)}')
