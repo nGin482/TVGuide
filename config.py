c5777d1425bac803c49f490422e22296b2245aa4
nGin482 <ngin2547@gmail.com>
2023-04-07 16:03:57 +1000

    Scheduler for sending Discord messages Remove imports no longer needed

--- config.py
+++ config.py
@@ -1,8 +1,10 @@
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from dotenv import load_dotenv
+
 from database.DatabaseService import DatabaseService
 from database.mongo import mongo_client
-from discord.ext.commands import Bot
-from dotenv import load_dotenv
 
 load_dotenv('.env')
 
 database_service = DatabaseService(mongo_client().get_database('tvguide'))
+scheduler = AsyncIOScheduler()
