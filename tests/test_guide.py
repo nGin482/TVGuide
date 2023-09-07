8da7f1e9958e787b2061b62d1d69aac413efa9e4
nGin482 <ngin2547@gmail.com>
2023-09-07 14:51:16 +1000

    More tests for season unknown GuideShow

--- tests/test_guide.py
+++ tests/test_guide.py
@@ -2,6 +2,7 @@ import unittest
 import json
 
 from database.models.GuideShow import GuideShow
+from database.models.RecordedShow import RecordedShow
 
 
 class TestGuide(unittest.TestCase):
@@ -9,100 +10,59 @@ class TestGuide(unittest.TestCase):
     def setUp(self):
         with open('tests/test_data/test_guide_list.json') as fd:
             self.data = json.load(fd)
+        with open('tests/test_data/recorded_shows.json') as fd:
+            temp_data = json.load(fd)
+            self.recorded_shows: list['RecordedShow'] = []
+            for recorded_show in temp_data:
+                self.recorded_shows.append(RecordedShow.from_database(recorded_show))
 
-    def test_get_transformers_1_movie(self):
-        transformers_data = next(show for show in self.data if show['title'] == 'Transformers')
-        title, season_number, episode_number, episode_title = GuideShow.get_show(
-            transformers_data['title'],
-            transformers_data['season_number'],
-            transformers_data['episode_number'],
-            transformers_data['episode_title']
-        )
-        self.assertEqual('Transformers', title)
-        self.assertEqual('1', season_number)
-        self.assertEqual(1, episode_number)
-        self.assertEqual('Transformers', episode_title)
-
-    def test_get_transformers_2_movie(self):
-        transformers_data = next(show for show in self.data if show['title'] == 'Transformers: Revenge of the Fallen')
-        title, season_number, episode_number, episode_title = GuideShow.get_show(
-            transformers_data['title'],
-            transformers_data['season_number'],
-            transformers_data['episode_number'],
-            transformers_data['episode_title']
-        )
-        self.assertEqual('Transformers', title)
-        self.assertEqual('1', season_number)
-        self.assertEqual(2, episode_number)
-        self.assertEqual('Revenge of the Fallen', episode_title)
-
-    def test_get_transformers_3_movie(self):
-        transformers_data = next(show for show in self.data if show['title'] == 'Transformers: Dark of the Moon')
-        title, season_number, episode_number, episode_title = GuideShow.get_show(
-            transformers_data['title'],
-            transformers_data['season_number'],
-            transformers_data['episode_number'],
-            transformers_data['episode_title']
-        )
-        self.assertEqual('Transformers', title)
-        self.assertEqual('1', season_number)
-        self.assertEqual(3, episode_number)
-        self.assertEqual('Dark of the Moon', episode_title)
-
-    def test_get_transformers_4_movie(self):
-        transformers_data = next(show for show in self.data if show['title'] == 'Transformers: Age of Extinction')
-        title, season_number, episode_number, episode_title = GuideShow.get_show(
-            transformers_data['title'],
-            transformers_data['season_number'],
-            transformers_data['episode_number'],
-            transformers_data['episode_title']
-        )
-        self.assertEqual('Transformers', title)
-        self.assertEqual('1', season_number)
-        self.assertEqual(4, episode_number)
-        self.assertEqual('Age of Extinction', episode_title)
-
-    def test_get_transformers_5_movie(self):
-        transformers_data = next(show for show in self.data if show['title'] == 'Transformers: The Last Knight')
-        title, season_number, episode_number, episode_title = GuideShow.get_show(
-            transformers_data['title'],
-            transformers_data['season_number'],
-            transformers_data['episode_number'],
-            transformers_data['episode_title']
-        )
-        self.assertEqual('Transformers', title)
-        self.assertEqual('1', season_number)
-        self.assertEqual(5, episode_number)
-        self.assertEqual('The Last Knight', episode_title)
-
-    def test_get_bumblebee_movie(self):
-        transformers_data = next(show for show in self.data if show['title'] == 'Bumblebee')
-        title, season_number, episode_number, episode_title = GuideShow.get_show(
-            transformers_data['title'],
-            transformers_data['season_number'],
-            transformers_data['episode_number'],
-            transformers_data['episode_title']
-        )
-        self.assertEqual('Transformers', title)
-        self.assertEqual('1', season_number)
-        self.assertEqual(6, episode_number)
-        self.assertEqual('Bumblebee', episode_title)
-
-    @unittest.skip('Test not yet set')
-    def test_get_doctor_who_show(self):
-        pass
-
-    @unittest.skip('Need to handle no RecordedShow object + mock RecordedShow class')
-    def test_season_known(self):
+    def test_season_known_repeat_true(self):
         dw_episode = self.data[0]
+        dw_recorded_show = next((show for show in self.recorded_shows if show.title == 'Doctor Who'), None)
         known_episode = GuideShow.known_season(
             dw_episode['title'],
             (dw_episode['channel'], dw_episode['time']),
             (dw_episode['season_number'], dw_episode['episode_number'], dw_episode['episode_title']),
-            None
+            dw_recorded_show
         )
-        self.assertEqual('Doctor Who', known_episode.title)
-        self.assertEqual('ABC2', known_episode.channel)
+        self.assertEqual(True, known_episode.repeat)
+
+    def test_season_known_repeat_false(self):
+        episode_data = next(show for show in self.data if show['episode_title'] == 'A Good Man Goes to War')
+        dw_recorded_show = next((show for show in self.recorded_shows if show.title == 'Doctor Who'), None)
+        known_episode = GuideShow.known_season(
+            episode_data['title'],
+            (episode_data['channel'], episode_data['time']),
+            (episode_data['season_number'], episode_data['episode_number'], episode_data['episode_title']),
+            dw_recorded_show
+        )
+        self.assertEqual(False, known_episode.repeat)
+
+    def test_season_unknown_no_details(self):
+        episode_data = next(show for show in self.data if show['episode_title'] == '')
+        unknown_episode = GuideShow.unknown_season(
+            episode_data['title'],
+            (episode_data['channel'], episode_data['time']),
+            episode_data['episode_title'],
+            None,
+            2
+        )
+        self.assertEqual(2, unknown_episode.episode_number)
+        self.assertEqual('Unknown', unknown_episode.season_number)
+
+    def test_season_unknown_no_details_2(self):
+        episode_data = next(show for show in self.data if show['episode_title'] == '')
+        dw_recorded_show = next((show for show in self.recorded_shows if show.title == 'Doctor Who'), None)
+        unknown_episode = GuideShow.unknown_season(
+            episode_data['title'],
+            (episode_data['channel'], episode_data['time']),
+            episode_data['episode_title'],
+            dw_recorded_show,
+            2
+        )
+        self.assertEqual(5, unknown_episode.episode_number)
+        self.assertEqual('Unknown', unknown_episode.season_number)
+
 
 
 if __name__ == '__main__':
