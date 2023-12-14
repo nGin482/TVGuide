951ac20011176233050ebb6968fd9101ff15d06c
nGin482 <ngin2547@gmail.com>
2023-12-14 21:32:19 +1100

    TVG-17 DB model for the search items

---
+++ database/models/SearchItem.py
@@ -0,0 +1,22 @@
+
+
+class SearchItem:
+
+    def __init__(self, show: str, image: str, conditions, search_active: bool):
+        self.show = show
+        self.image = image
+        self.conditions = conditions
+        self.search_active = search_active
+
+    @classmethod
+    def from_database(cls, search_item: dict[str]):
+        return cls(search_item['show'], search_item['image'], search_item['conditions'], search_item['search_active'])
+
+    
+    def to_dict(self):
+        return {
+            'show': self.show,
+            'image': self.image,
+            'conditions': self.conditions,
+            'search_active': self.search_active
+        }
