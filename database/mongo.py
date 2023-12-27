5ce22aa6a369195e81e1f50423a55e77f38c32ee
nGin482 <ngin2547@gmail.com>
2023-12-27 17:01:52 +1100

    TVG-18 Connecting to local Mongo database

--- database/mongo.py
+++ database/mongo.py
@@ -1,15 +1,22 @@
 from pymongo import MongoClient, errors
+import sys
 import os
 
 def mongo_client():
     try:
-        return MongoClient(os.getenv('TVGUIDE_DB'))
+        connection_string = db_connection_string()
+        return MongoClient(connection_string)
     except (errors.ConfigurationError, errors.ServerSelectionTimeoutError) as e:
         from services.hermes.hermes import hermes
         print('Having trouble connecting to the database.')
         print(e)
         hermes.dispatch('db_not_connected', str(e))
         
+def db_connection_string():
+    if len(sys.argv) > 1 and '--local-db' in sys.argv[1]:
+        return os.getenv('LOCAL_DB')
+    else:
+        return os.getenv('TVGUIDE_DB')
 
 def database():
     mongo = mongo_client()
