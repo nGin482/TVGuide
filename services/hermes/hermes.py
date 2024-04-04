8fe4a5e28e345679867bd42e44938db366f61e3b
nGin482 <ngin2547@gmail.com>
2024-04-04 22:57:41 +1100

    TVG-34 Cleanup old env loading in hermes initialiser

--- services/hermes/hermes.py
+++ services/hermes/hermes.py
@@ -1,11 +1,5 @@
 from discord.ext.commands import Bot, DefaultHelpCommand
-from dotenv import load_dotenv
-import os
 
-if os.environ['PYTHON_ENV'] == 'production':
-    load_dotenv('.env')
-else:
-    load_dotenv('.env.local.dev')
 hermes = Bot(command_prefix='$', help_command=DefaultHelpCommand())
 
 
