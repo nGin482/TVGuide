bcfe049a991ffaca75647e2c7b39cccd0b030995
nGin482 <ngin2547@gmail.com>
2024-05-28 23:20:59 +1000

    TVG-40 API client for making requests

---
+++ services/APIClient.py
@@ -0,0 +1,30 @@
+from typing import TypeVar
+import requests
+
+TBody = TypeVar('TBody', list, dict)
+
+class APIClient:
+
+
+    @staticmethod
+    def _make_request(method: str, url: str, additional_headers: dict):
+
+        headers = {
+            'User-Agent': 'Chrome/119.0.0.0'
+        }
+
+        headers.update(additional_headers)
+        
+        response = requests.request(
+            method=method,
+            url=url,
+            headers=headers
+        )
+
+        return response
+    
+    def get(self, endpoint: str, headers: dict = {}) -> TBody:
+        
+        response = APIClient._make_request('GET', endpoint, headers)
+
+        return response.json()
