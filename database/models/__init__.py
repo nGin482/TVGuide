e6b42b6cf3c097bec548967f01f1e494904e11f4
nGin482 <ngin2547@gmail.com>
2024-08-22 11:38:12 +1000

    TVG-49 Common location to import models from

---
+++ database/models/__init__.py
@@ -0,0 +1,5 @@
+from database.models.GuideEpisode import GuideEpisode
+from database.models.Reminder import Reminder
+from database.models.SearchItemModel import SearchItem
+from database.models.ShowDetails import ShowDetails
+from database.models.ShowEpisode import ShowEpisode
