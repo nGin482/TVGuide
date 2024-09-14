49d23bff36437d45d78443c54f2cf9794748b24e
nGin482 <ngin2547@gmail.com>
2024-09-14 17:05:37 +1000

    TVG-49 Update API to use relational DB

--- api.py
+++ api.py
@@ -1,25 +1,17 @@
+from datetime import datetime
+from dotenv import load_dotenv
 from flask import Flask, request, render_template, send_from_directory
 from flask_cors import CORS
 from flask_jwt_extended import create_access_token, JWTManager, jwt_required, get_current_user
+from sqlalchemy.orm import Session
 import sys
 import os
 
-if len(sys.argv) > 1:
-    os.environ['PYTHON_ENV'] = sys.argv[1]
-else:
-    os.environ['PYTHON_ENV'] = 'production'
-from config import database_service
-from database.models.RecordedShow import RecordedShow, Season, Episode
-from database.models.Reminders import Reminder
-from database.models.SearchItem import SearchItem
-from database.models.Users import User
-from exceptions.DatabaseError import (
-    DatabaseError,
-    InvalidSubscriptions,
-    SearchItemAlreadyExistsError,
-    SearchItemNotFoundError,
-    UserNotFoundError
-)
+load_dotenv('.env')
+from database.database import engine
+from database.models import Reminder, SearchItem, ShowDetails, ShowEpisode, User, UserSearchSubscription
+from database.models.GuideModel import Guide
+from exceptions.DatabaseError import DatabaseError, InvalidSubscriptions
 from services.tvmaze.tvmaze_api import get_show_data
 
 app = Flask(__name__, template_folder='build', static_folder='build/static')
@@ -32,7 +24,8 @@ jwt = JWTManager(app)
 
 @jwt.user_lookup_loader
 def user_lookup_callback(_jwt_header, jwt_data):
-    return database_service.get_user(jwt_data['sub'])
+    session = Session(engine)
+    return User.search_for_user(jwt_data['sub'], session)
 
 @app.route('/')
 def index():
@@ -49,7 +42,7 @@ def manifest():
 # SEARCH LIST
 @app.route('/api/show-list', methods=['GET'])
 def show_list():
-    return [item.to_dict() for item in database_service.get_search_list()]
+    return [item.to_dict() for item in SearchItem.get_active_searches()]
 
 @app.route('/api/show-list', methods=['POST'])
 @jwt_required()
@@ -57,106 +50,78 @@ def add_show_list():
     user: User = get_current_user()
     print(user.username)
     body = request.json
-    if 'show' not in body or 'tvmaze_id' not in body or 'image' not in body or 'conditions' not in body:
-        return {'message': "Please provide the show's name, the search conditions and the id and image from TVMaze"}, 400
-    show, image, tvmaze_id, conditions = body['show'], body['image'], body['tvmaze_id'], body['conditions']
-    if show == '' or image == '' or tvmaze_id == '':
-        return {'message': "Please provide the show's name and the Id and image from TVMaze"}, 400
-    try:
-        new_search_item = SearchItem(show, image, conditions, True)
-        database_service.add_search_item(new_search_item)
-        new_show_data = get_show_data(show, tvmaze_id)
-        recorded_show = RecordedShow.from_database(new_show_data)
-        database_service.insert_recorded_show_document(recorded_show)
-        return {'message': f'{show} was added to the Search List'}
-    except SearchItemAlreadyExistsError as err:
-        return {'message': str(err)}, 409
-    except DatabaseError as err:
-        return {'message': str(err)}, 500
+    
+    if 'show' not in body or body['show'] == '':
+        return { 'message': "Please provide the name of the show to add the Search Item" }, 400
+    show, conditions = body['show'], body['conditions']
+
+    show_details_check = ShowDetails.get_show_by_title(show)
+    search_item_check = SearchItem.get_search_item(show)
+
+    if show_details_check is None:
+        return { 
+            'message': f"No details about '{show}' can be found. Please add details about the show before adding the Search Item"
+        }, 400
+    if search_item_check:
+        return { 'message': f"A Search Item already exists for '{show}'" }, 409
+
+    new_search_item = SearchItem(show, False, conditions)
+    new_search_item.add_search_item()
+    # new_show_data = get_show_data(show, tvmaze_id)
 
 @app.route('/api/show-list/<string:show>', methods=['DELETE'])
 @jwt_required()
 def delete_search_item(show: str):
-    try:
-        database_service.remove_show_from_list(show)
+    search_item = SearchItem.get_search_item(show)
+    if search_item:
+        search_item.delete_search()
         return {'message': f'{show} was deleted from the Search List'}
-    except SearchItemNotFoundError as err:
-        return { 'message': str(err) }, 404
-    except DatabaseError as err:
-        return { 'message': str(err) }, 500
+    return { 'message': f"No search item could be found for '{show}'" }, 404
         
 # GUIDE
 @app.route('/api/guide')
 def guide():
+    from data_validation.validation import Validation
     if request.args.get('date'):
-        date = request.args.get('date')
-        guide = database_service.get_guide_date(date)
-        if not guide:
-            return {'message': f'No guide data has been found for {date}'}, 404
+        dates = request.args.get('date').split('/')
+        date = datetime(year=int(dates[2]), month=int(dates[1]), day=int(dates[0]))
     else:
-        guide = database_service.get_latest_guide()
+        date = Validation.get_current_date()
+    guide = Guide(date)
+    guide.get_shows()
     return guide.to_dict()
 
 # RECORDED SHOWS
 @app.route('/api/recorded-shows')
 def recorded_shows():
-    recorded_shows = [recorded_show.to_dict() for recorded_show in database_service.get_all_recorded_shows()]
+    recorded_shows = [show.to_dict() for show in ShowDetails.get_all_shows()]
     return recorded_shows
 
-@app.route('/api/recorded-shows/<string:show>')
-def get_recorded_show(show: str):
-    recorded_show = database_service.get_one_recorded_show(show)
-    if recorded_show:
-        return recorded_show.to_dict()
-    return {'message': f'A recorded show for {show} could not be found'}, 404
-
-@app.route('/api/recorded-show/<string:show>', methods=['PUT', 'DELETE'])
+@app.route('/api/recorded-show/<string:show>', methods=['GET', 'PUT', 'DELETE'])
 @jwt_required()
 def recorded_show(show: str):
-    recorded_show = database_service.get_one_recorded_show(show)
-    if recorded_show:
+    show_details = ShowDetails.get_show_by_title(show)
+    if show_details:
+        if request.method == 'GET':
+            return show_details.to_dict()
         if request.method == 'PUT':
-            season_query = request.args.get('season')
-            episode_query = request.args.get('episode')
-            if season_query and episode_query:
-                # update episode
-                episode = Episode.from_database(request.json)
-                database_service.update_episode_in_database(show, season_query, episode)
-                return {'message': 'The episode has been updated'}
-            elif season_query:
-                # add episode to season
-                episode = Episode.from_database(request.json)
-                database_service.add_new_episode_to_season(recorded_show, season_query, episode)
-                return {'message': f'The episode has been added to Season {season_query} of {recorded_show.title}'}
-            else:
-                # add season
-                season = Season.from_database(request.json)
-                database_service.add_new_season(recorded_show, season)
-                return {'message': f'The season has been added to {recorded_show.title}'}
+            body: dict = request.json
+            for key in body.keys():
+                show_details.update_show(key, body[key])
+            return { 'show_details': show_details.to_dict() }
         if request.method == 'DELETE':
             current_user: User = get_current_user()
             if current_user.role != 'Admin':
-                return {'message': f'You are not authorised to delete {show}. Please make a request to delete it'}, 403
-            season_query = request.args.get('season')
-            episode_query = request.args.get('episode')
-            if season_query and episode_query:
-                # delete episode
-                database_service.remove_episode_from_season(show, season_query, int(episode_query))
-                return {'message': f'Episode {episode_query} has been removed from Season {season_query} of {show}'}
-            elif season_query:
-                # delete season
-                return {'message': 'No action performed'}
-            else:
-                # delete show
-                database_service.delete_recorded_show(show)
-                return {'message': f'{show} has been deleted'}
-    return {'message': f'A recorded show for {show} could not be found'}, 404
+                return { 'message': f"You are not authorised to delete '{show}'. Please make a request to delete it" }, 403
+            show_details.delete_show()
+            return {'message': f'{show} has been deleted'}
+    return {'message': f"Details for '{show}' could not be found"}, 404
 
 # REMINDERS
 @app.route('/api/reminders')
 def get_reminders():
     try:
-        reminders = [reminder.to_dict() for reminder in database_service.get_all_reminders()]
+        reminders = [reminder.to_dict() for reminder in Reminder.get_all_reminders()]
         return reminders
     except (KeyError, ValueError) as error:
         return {'message': 'There was a problem retrieving the reminders', 'error': str(error)}, 500
@@ -164,27 +129,26 @@ def get_reminders():
 @app.route('/api/reminders', methods=['POST'])
 @jwt_required()
 def reminders():
-    reminder = request.json
-    show: str = reminder['show']
-    show_check = show in [show.title for show in database_service.get_all_recorded_shows()]
-    reminder_check = database_service.get_one_reminder(show)
+    body = request.json
+    show: str = body['show']
+    show_check = ShowDetails.get_show_by_title(show)
+    reminder_check = Reminder.get_reminder_by_show(show)
     if not show_check:
         return {'message': f'{show} is not being searched for'}, 400
     if reminder_check:
         return {'message': f'A reminder already exists for {show}'}, 409
-    new_reminder = Reminder.from_database(reminder)
+    new_reminder = Reminder(show, body['alert'], body['warning_time'], body['occasions'])
     try:
-        database_service.insert_new_reminder(new_reminder)
+        new_reminder.add_reminder()
         return {
-            'message': f'Your reminder for {show} has been created',
-            'reminders': [reminder.to_dict() for reminder in database_service.get_all_reminders()]
+            'reminders': new_reminder.to_dict()
         }
     except DatabaseError as err:
         return {'message': f'An error occurred creating the reminder for {show}', 'error': str(err)}, 500
     
 @app.route('/api/reminder/<string:show>')
 def get_reminder(show: str):
-    reminder = database_service.get_one_reminder(show)
+    reminder = Reminder.get_reminder_by_show(show)
     if reminder:
         return reminder.to_dict()
     return {'message': f'A reminder for {show} does not exist'}, 404
@@ -192,50 +156,47 @@ def get_reminder(show: str):
 @app.route('/api/reminder/<string:show>', methods=['PUT', 'DELETE'])
 @jwt_required()
 def reminder(show: str):
-    reminder = database_service.get_one_reminder(show)
+    reminder = Reminder.get_reminder_by_show(show)
     if reminder:
         if request.method == 'PUT':
             body = dict(request.json)
-            updated_reminder = Reminder.from_database(body)
-            database_service.update_reminder(updated_reminder)
+            updated_reminder = Reminder.get_reminder_by_show(show)
+            for key in body.keys():
+                setattr(updated_reminder, key, body[key])
             return updated_reminder.to_dict()
         if request.method == 'DELETE':
             current_user: User = get_current_user()
             if current_user.role != 'Admin':
                 return {'message': f'You are not authorised to delete this reminder. Please make a request to delete it'}, 403
-            database_service.delete_reminder(show)
-            reminders = [reminder.to_dict() for reminder in database_service.get_all_reminders()]
-            return {'message': f'The reminder for {show} has been deleted', 'reminders': reminders}
+            reminder = Reminder.get_reminder_by_show(show)
+            reminder.delete_reminder()
+            reminders = [reminder.to_dict() for reminder in Reminder.get_all_reminders()]
+            return { 'reminders': reminders }
     return {'message': f'A reminder for {show} does not exist'}, 404
 
 # USERS
 @app.route('/api/user/<string:username>', methods=['GET'])
 def get_user(username: str):
-    user = database_service.get_user(username)
+    session = Session(engine)
+    user = User.search_for_user(username, session)
     if user:
-        user_data = user.to_dict()
-        del user_data['password']
-        return user_data
+        return user.to_dict()
     return {'message': f'An account with the username {username} could not be found'}, 404
 
 @app.route('/api/users/<string:username>/subscriptions', methods=['PUT'])
 @jwt_required()
 def edit_user_subscriptions(username: str):
-    user = database_service.get_user(username)
+    session = Session(engine)
+    user = User.search_for_user(username, session)
     if user:
         current_user: User = get_current_user()
         if current_user.username != username:
-            return {'message': "You are not authorised to update this user's details"}, 403
+            return {'message': "You are not able to update this user's details"}, 403
         body = request.json
         try:
-            user_was_updated = database_service.update_user_subscriptions(
-                username,
-                body['operation'],
-                body['resource'],
-                body['subscriptions']
-            )
-            if user_was_updated:
-                return {'message': 'Your subscriptions were updated', 'user': user_was_updated}
+            user_subscription = UserSearchSubscription(user.id, body['search_item_id'])
+            user_subscription.add_subscription(session)
+            return { 'user': user.to_dict() }
         except InvalidSubscriptions as err:
             return {'message': str(err)}, 400            
     return {'message': f'A user with the username {username} could not be found'}, 404
@@ -243,55 +204,65 @@ def edit_user_subscriptions(username: str):
 @app.route('/api/user/<string:username>/promote', methods=['PATCH'])
 @jwt_required()
 def promote_user(username: str):
+    session = Session(engine, expire_on_commit=False)
     current_user: User = get_current_user()
     if current_user.role == 'Admin':
-        try:
-            promoted_user = database_service.promote_user(username)
-            return promoted_user
-        except UserNotFoundError as err:
-            return { 'message': str(err) }, 404
+        user = User.search_for_user(username, session)
+        if user:
+            user.promote_role()
+            session.commit()
+            session.close()
+            return '', 204
+        session.close()
+        return { 'message': f"Unable to find the user '{username}'" }, 404
     return { 'message': 'You are not authorised to promote this user to an admin role' }, 403
     
 @app.route('/api/user/<string:username>/change_password', methods=['POST'])
 @jwt_required()
 def change_password(username: str):
     current_user: User = get_current_user()
-    if current_user.username == username:
-        database_service.change_user_password(username, request.json['password'])
+    session = Session(engine)
+    user = User.search_for_user(username, session)
+    if user and current_user.username == user.username:
+        user.change_password(request.json['passowrd'])
         return { 'message': 'Your password has been updated' }
     return { 'message': "You are not authorised to change this user's password" }, 403
 
 @app.route('/api/user/<string:username>', methods=['DELETE'])
 @jwt_required()
 def delete_user(username: str):
+    session = Session(engine)
     current_user: User = get_current_user()
+    user = User.search_for_user(username, session)
     if current_user.username == username:
-        database_service.delete_user(username)
+        user.delete_user(session)
         return { 'message': 'Your account has been deleted' }
     elif current_user.role == 'Admin':
-        check_user = database_service.get_user(username)
-        if check_user:
-            database_service.delete_user(username)
-            return { 'message': 'Your account has been deleted' }
+        if user:
+            user.delete_user(session)
+            return { 'message': 'The account has been deleted' }
         else:
-            return {'message': f'An account with the username {username} could not be found'}, 404
+            return {'message': f"An account with the username '{username}' could not be found"}, 404
     else:
         return { 'message': 'You are not authorised to delete this user account' }
 
 @app.route('/api/auth/register', methods=['POST'])
-def registerUser():
+def register_user():
     body = request.json
-    check_user = database_service.get_user(body['username'])
+    session = Session(engine)
+    check_user = User.search_for_user(body['username'], session)
     if check_user:
         return {'message': 'This username is already in use'}, 409
     print(body)
-    database_service.register_user(body['username'], body['password'], body['show_subscriptions'], body['reminder_subscriptions'])
+    user = User(body['username'], body['password'])
+    user.add_user(session)
     return {'message': 'You have successfully been registered'}
 
 @app.route('/api/auth/login', methods=['POST'])
 def login():
+    session = Session(engine)
     given_credentials = request.json
-    user = database_service.get_user(given_credentials['username'])
+    user = User.search_for_user(given_credentials['username'], session)
     if user and user.check_password(given_credentials['password']):
         return {
             'user': {
@@ -300,17 +271,7 @@ def login():
                 'token': create_access_token(identity=user.username)
             }
         }
-    return {'message': 'Incorrect username or password'}, 401
-
-@app.route('/api/events')
-@jwt_required()
-def events():
-    user: User = get_current_user()
-    if user.role != 'Admin':
-        return {'message': 'You are not authorised to retrieve events'}, 403
-    guide = database_service.get_latest_guide()
-    events = [show.to_dict() for show in guide.fta_shows]
-    return events
+    return { 'message': 'Incorrect username or password' }, 401
 
 if __name__ == '__main__':
     if os.getenv('PYTHON_ENV') == 'production':
