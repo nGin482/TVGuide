fde71d8360b06836746613a5493b688276694ec9
nGin482 <ngin2547@gmail.com>
2025-02-22 22:53:45 +1100

    TVG-87 Read env variables from correct .env file

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import { fileURLToPath } from "url";
 
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, `../`, "");
+    const env = loadEnv(mode, `./`, "");
     return {
         plugins: [
             react()
