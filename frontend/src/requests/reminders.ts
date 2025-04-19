80afa639f5b8bbcffb6f0a12f388302138f2386a
nGin482 <ngin2547@gmail.com>
2025-04-19 14:13:58 +1000

    TVG-103 Export reminder requests

--- frontend/src/requests/reminders.ts
+++ frontend/src/requests/reminders.ts
@@ -2,24 +2,27 @@
 import { deleteRequest, getRequest, postRequest, putRequest } from "./api-client";
 import { Reminder, ReminderFormValues } from "../utils/types";
 
-const getReminders = async () => {
+export const getReminders = async () => {
     const reminders = await getRequest<Reminder[]>("/reminders");
     return reminders;
 };
-const addReminder = async (reminder: ReminderFormValues, token: string) => {
+
+export const addReminder = async (reminder: ReminderFormValues, token: string) => {
     return await postRequest<ReminderFormValues, Reminder>(
         `/reminders`,
         reminder,
         { Authorization: `Bearer ${token}` }
     );
 };
-const editReminder = async (reminderDetails: ReminderFormValues, token: string) => {
+
+export const editReminder = async (reminderDetails: ReminderFormValues, token: string) => {
     return await putRequest<ReminderFormValues, Reminder>(
         `/reminder/${reminderDetails.show}`,
         reminderDetails,
         { Authorization: `Bearer ${token}` }
     );
-}
-const deleteReminder = async (show: string, token: string) => {
+};
+
+export const deleteReminder = async (show: string, token: string) => {
     return await deleteRequest(`/reminder/${show}`, { Authorization: `Bearer ${token}` });
 };
