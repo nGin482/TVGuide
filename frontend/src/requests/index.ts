e0c6126614965975dd3c8b7dd94511fd9048df9b
nGin482 <ngin2547@gmail.com>
2025-04-19 14:23:47 +1000

    TVG-103 Move show episode requests into new file

--- frontend/src/requests/index.ts
+++ frontend/src/requests/index.ts
@@ -1,33 +1,14 @@
 
-import { putRequest } from "./api-client";
-import {
-    ShowEpisode,
-} from "../utils/types";
-
-
-
-
-
-
-const updateShowEpisode = async (episode: ShowEpisode, token: string) => {
-    return await putRequest<ShowEpisode, ShowEpisode>(
-        `/show-episode/${episode.id}`,
-        episode,
-        { Authorization: `Bearer ${token}` }
-    );
-};
 
 
 export * from "./auth";
 export * from "./guide";
 export * from "./reminders";
 export * from "./shows";
+export * from "./show-episode";
 export * from "./search-items";
 export * from "./search-subscriptions";
 export * from "./user";
 
-export {
-    updateShowEpisode,
-};
 
 export * from './tvmaze';
