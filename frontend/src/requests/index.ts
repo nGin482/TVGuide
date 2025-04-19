71c5e0a4a4e18e467cc43e49cad9c42b2a744298
nGin482 <ngin2547@gmail.com>
2025-04-19 14:19:27 +1000

    TVG-103 Move user searh subscription requests into new file

--- frontend/src/requests/index.ts
+++ frontend/src/requests/index.ts
@@ -1,9 +1,8 @@
 
-import { deleteRequest, getRequest, postRequest, putRequest } from "./api-client";
+import { getRequest, putRequest } from "./api-client";
 import {
     Guide,
     ShowEpisode,
-    User,
 } from "../utils/types";
 
 const getGuide = async () => {
@@ -21,39 +20,17 @@ const updateShowEpisode = async (episode: ShowEpisode, token: string) => {
     );
 };
 
-const getUserSubscriptions = async (user: string) => {
-    const data = await getRequest(`/users/${user}/subscriptions`);
-};
-const addSubscriptions = async (
-    username: string,
-    subscriptions: string[],
-    token: string
-) => {
-    const updatedUser = await postRequest<string[], User>(
-        `/users/${username}/subscriptions`,
-        subscriptions,
-        { Authorization: `Bearer ${token}` }
-    );
-
-    return updatedUser;
-};
-
-const unsubscribeFromSearch = async (subscriptionId: number, token: string) => {
-    await deleteRequest(`/users/subscriptions/${subscriptionId}`, { Authorization: `Bearer ${token}` });
-};
 
 export * from "./auth";
 export * from "./reminders";
 export * from "./shows";
 export * from "./search-items";
+export * from "./search-subscriptions";
 export * from "./user";
 
 export {
     getGuide,
     updateShowEpisode,
-    getUserSubscriptions,
-    addSubscriptions,
-    unsubscribeFromSearch,
 };
 
 export * from './tvmaze';
