e03f06bc9651d39856134762e844e6a81b4d3010
nGin482 <ngin2547@gmail.com>
2025-05-11 17:14:19 +1000

    TVG-108 Fix frontend tests

--- frontend/tests/TVGuide.test.tsx
+++ frontend/tests/TVGuide.test.tsx
@@ -1,67 +1,57 @@
-import { fireEvent, render, screen } from '@testing-library/react';
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
+import axios, { AxiosResponse } from 'axios';
+import dayjs from 'dayjs';
 
 import TVGuide from '../src/components/TVGuide';
 
 import { currentUser, guide } from './test_data';
 
-describe('test TVGuide component', () => {
-    test('renders service buttons', () => {
-        render(<TVGuide guide={guide} />);
-        const ftaButton = screen.getByText(/Free to Air/i);
-        const bbcButton = screen.getByText(/BBC Channels/i);
-        const allButton = screen.getByText(/All/i);
-        expect(ftaButton).toBeInTheDocument();
-        expect(bbcButton).toBeInTheDocument();
-        expect(allButton).toBeInTheDocument();
+jest.mock("axios");
+const mockedAxios = axios as jest.Mocked<typeof axios>;
+
+const response: AxiosResponse = {
+    status: 200,
+    statusText: 'OK',
+    data: null,
+    headers: null,
+    config: null
+};
+
+describe("test TVGuide component", () => {
+    beforeEach(() => {
+        jest.resetAllMocks();
     });
 
-    test('fta button only shows FTA guide', () => {
-        render(<TVGuide guide={guide} />);
-        const ftaButton = screen.getByText(/Free to Air/i);
+    test("renders guide data", async () => {
+        response.data = guide;
+                
+        mockedAxios.get.mockResolvedValue(response);
         
-        fireEvent.click(ftaButton);
+        const { getByRole } = render(<TVGuide />);
 
-        const maigret = screen.getByText(/^Maigret$/i);
-        const deathInParadise = screen.queryByText(/Death in Paradise/i);
-        expect(maigret).toBeInTheDocument();
-        expect(deathInParadise).not.toBeInTheDocument();
-    });
-
-    test('bbc button only shows BBC guide', () => {
-        render(<TVGuide guide={guide} />);
-        const bbcButton = screen.getByText(/BBC Channels/i);
+        const dateHeader = screen.getByRole("heading");
+        expect(dateHeader).toBeInTheDocument();
+        expect(dateHeader.innerHTML).toContain(dayjs().format("DD MMMM YYYY"));
         
-        fireEvent.click(bbcButton);
-
-        const maigret = screen.queryByText(/^Maigret$/i);
-        const deathInParadise = screen.queryByText(/Death in Paradise/i);
-        expect(maigret).not.toBeInTheDocument();
-        expect(deathInParadise).toBeInTheDocument();
+        await waitFor(() => expect(getByRole("table")).toBeInTheDocument());
     });
 
-    test('all button shows FTA shows and BBC shows', () => {
-        render(<TVGuide guide={guide} />);
-        const allButton = screen.getByText(/All/i);
-
-        fireEvent.click(allButton);
-
-        const maigret = screen.queryByText(/^Maigret$/i);
-        const deathInParadise = screen.queryByText(/Death in Paradise/i);
-        expect(maigret).toBeInTheDocument();
-        expect(deathInParadise).toBeInTheDocument();
+    test('TVGuide only renders shows user has subscribed to', async () => {
+        response.data = guide;              
+        mockedAxios.get.mockResolvedValue(response);
+        
+        render(<TVGuide user={currentUser} />);
+        
+        await waitFor(() => {
+            const maigret = screen.queryAllByText(/Maigret/i);
+            const deathInParadise = screen.queryByText(/Death in Paradise/i);
+            const vera = screen.queryByText(/Vera/i);
+            const lewis = screen.queryByText(/Lewis/i);
+    
+            expect(maigret[0]).toBeInTheDocument();
+            expect(deathInParadise).not.toBeInTheDocument();
+            expect(vera).not.toBeInTheDocument();
+            expect(lewis).not.toBeInTheDocument();
+        });
     });
-
-    test('TVGuide only renders shows user has sbscribed to', () => {
-        render(<TVGuide guide={guide} user={currentUser} />);
-
-        const maigret = screen.queryAllByText(/Maigret/i);
-        const deathInParadise = screen.queryByText(/Death in Paradise/i);
-        const vera = screen.queryByText(/Vera/i);
-        const lewis = screen.queryByText(/Lewis/i);
-
-        expect(maigret[0]).toBeInTheDocument();
-        expect(deathInParadise).not.toBeInTheDocument();
-        expect(vera).not.toBeInTheDocument();
-        expect(lewis).not.toBeInTheDocument();
-    })
 });
