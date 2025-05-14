dc139923e7a6fd4387b4f45264c6a6406a839e15
nGin482 <ngin2547@gmail.com>
2025-05-14 22:09:04 +1000

    TVG-108 TVGuide component tests for error handling and selecting a date

--- frontend/tests/TVGuide.test.tsx
+++ frontend/tests/TVGuide.test.tsx
@@ -36,6 +36,76 @@ describe("test TVGuide component", () => {
         await waitFor(() => expect(getByRole("table")).toBeInTheDocument());
     });
 
+    test("renders an error alert if no guide is returned", async () => {
+        const { getByRole } = render(<TVGuide />);
+        
+        await waitFor(() => {
+            const error = getByRole("alert");
+            expect(error).toBeInTheDocument();
+            expect(error.innerHTML).toContain(
+                `There was a problem fetching the guide for ${dayjs().format("DD MMMM YYYY")}`
+            );
+            expect(error.innerHTML).toContain(
+                "There is a problem communicating with the server"
+            );
+        });
+    });
+
+    test("renders an error alert if no guide is found for the date chosen", async () => {
+        const noGuideResponse: AxiosResponse = JSON.parse(JSON.stringify(response));
+        noGuideResponse.status = 404;
+        noGuideResponse.statusText = "Not Found";
+        noGuideResponse.data = {
+            message: "There is no guide for this date"
+        };
+                
+        mockedAxios.get.mockRejectedValue({ response: noGuideResponse });
+
+        const { getByRole } = render(<TVGuide />);
+        
+        await waitFor(() => {
+            const error = getByRole("alert");
+            expect(error).toBeInTheDocument();
+            expect(error.innerHTML).toContain(
+                `There was a problem fetching the guide for ${dayjs().format("DD MMMM YYYY")}`
+            );
+            expect(error.innerHTML).toContain("There is no guide for this date");
+        });
+    });
+
+    test("renders a datepicker to choose a different date", async () => {
+        response.data = guide;              
+        mockedAxios.get.mockResolvedValue(response);
+        
+        const { getByRole } = render(<TVGuide />);
+
+        const datePicker = getByRole("textbox");
+        expect(datePicker).toBeInTheDocument();
+    });
+
+    test("datepicker changes date displayed", async () => {
+        response.data = guide;              
+        mockedAxios.get.mockResolvedValue(response);
+        
+        const { getByRole, rerender } = render(<TVGuide />);
+
+        const previousDay = dayjs().subtract(1, "day");
+
+        const datePicker = getByRole("textbox");
+        expect(datePicker).toBeInTheDocument();
+        fireEvent.mouseDown(datePicker);
+        fireEvent.change(datePicker, { target: { value: previousDay.format("YYYY-MM-DD") } });
+        expect(datePicker).toHaveValue(previousDay.format("YYYY-MM-DD"));
+        
+        await waitFor(() => {
+            rerender(<TVGuide />);
+
+            const dateHeader = getByRole("heading");
+            expect(dateHeader).toBeInTheDocument();
+        });
+        
+    });
+
     test('TVGuide only renders shows user has subscribed to', async () => {
         response.data = guide;              
         mockedAxios.get.mockResolvedValue(response);
