529af23df07efc3a3f0281472ba26b5722787c3f
nGin482 <ngin2547@gmail.com>
2026-08-02 13:15:33 +1000

    TVG-122 Cleanup old and unused code

--- log.py
+++ log.py
@@ -1,14 +1,9 @@
 from __future__ import annotations
 from datetime import datetime
 import logging
-import json
-import os
 import re
 
 from data_validation.validation import Validation
-from typing import TYPE_CHECKING
-if TYPE_CHECKING:
-    from database.models.GuideShow import GuideShow
 
 
 def get_date_from_tvguide_message(message: str):
@@ -35,22 +30,6 @@ def compare_dates(date: datetime):
             return True
         return False
 
-
-def log_discord_message_too_long(message_length, fta_length):
-
-    today_date = datetime.today().strftime("%d-%m-%Y")
-    log_message = f'{today_date}\nDiscord tried sending the TVGuide message.\nThe maximum character length of a Discord message is 2000.\n \
-        The length of this message is {message_length} and the length of the Free to Air portion is {fta_length}.\n \
-            As a result, the message was split into the Free to Air and BBC portions and on the AM/PM portions of Free to Air.' 
-
-    try:
-        with open('log/message_logging.txt') as fd:
-            current_log_contents = fd.read()
-    except FileNotFoundError:
-        current_log_contents = ''
-            
-    with open('log/message_logging.txt', 'w', encoding='utf-8') as fd:
-        fd.write(f'{current_log_contents}\n\n\n{log_message}')
 
 def logging_app(log_info: str, level = logging.DEBUG):
     logging.basicConfig(filename='tvguide.log', filemode='a', level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
