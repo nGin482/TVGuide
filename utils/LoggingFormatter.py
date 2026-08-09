c2b91ada091678f19d4a50ebabf13229035cf70f
nGin482 <nicholasgiumelli@gmail.com>
2026-08-09 20:45:37 +1000

    TVG-123 Change formatting of logs

--- utils/LoggingFormatter.py
+++ utils/LoggingFormatter.py
@@ -1,24 +1,10 @@
-
-
 import logging
-import json
 import sys
 
-class LoggingFormatter(logging.Formatter):
-    def format(self, record):
-        log_record = {
-            "timestamp": self.formatTime(record, self.datefmt),
-            "level": record.levelname, 
-            "severity_text": record.levelname,
-            "message": record.getMessage(),
-            "logger_name": record.name,
-        }
-        # Include exception info if it exists
-        if record.exc_info:
-            log_record["exc_info"] = self.formatException(record.exc_info)
-            
-        return json.dumps(log_record)
-
+dt_fmt = '%Y-%m-%d %H:%M:%S'
+formatter = logging.Formatter(
+    '[{asctime}] [{levelname}] {name}: {message}', dt_fmt, style='{'
+)
 
 logging_handler = logging.StreamHandler(sys.stdout)
-logging_handler.setFormatter(LoggingFormatter())
+logging_handler.setFormatter(formatter)
