20fd6954655f9910fb32f9edd90c51f9469de7ca
nGin482 <nicholasgiumelli@gmail.com>
2026-08-13 18:53:46 +1000

    TVG-112 Formatting

--- frontend/src/requests/show-episode.ts
+++ frontend/src/requests/show-episode.ts
@@ -3,5 +3,8 @@ import { putRequest } from "./api-client";
 import { ShowEpisode } from "../utils/types";
 
 export const updateShowEpisode = async (episode: ShowEpisode) => {
-    return await putRequest<ShowEpisode, ShowEpisode>(`/show-episode/${episode.id}`, episode);
+  return await putRequest<ShowEpisode, ShowEpisode>(
+    `/show-episode/${episode.id}`,
+    episode
+  );
 };
